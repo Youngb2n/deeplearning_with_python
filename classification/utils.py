4845eae68afd2bba52284e6b1e13712165a27b4d
AhnYoungbin <69440939+Youngb2n@users.noreply.github.com>
2020-11-11 07:10:04 +0900

    Update utils.py

--- classification/utils.py
+++ classification/utils.py
@@ -16,7 +16,7 @@ def save_checkpoint(state, is_best, history_path):
         shutil.copyfile(filename, best_filename)
         
         
-def adjust_learning_rate_(optimizer, epoch, start_lr):
+def adjust_learning_rate(optimizer, epoch, start_lr):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
     lr = start_lr * (0.1 ** (epoch // 30))
     for param_group in optimizer.param_groups:
